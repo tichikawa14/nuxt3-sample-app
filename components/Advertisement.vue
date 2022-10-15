665137a42ebfc718d037c586835c13c02cefa9f6
tichikawa14 <gakusei.umeeee@gmail.com>
2022-10-15 22:43:15 +0900

    feat: 簡単なNuxtアプリケーションを作成

---
+++ components/Advertisement.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const ads = ref<{ id: number, title: string, url: string }[]>([]);
+ads.value = [{
+  id: 1,
+  title: "エンジニア募集中",
+  url: "https://tandems.co.jp//"
+}, {
+  id: 2,
+  title: "オンラインセミナー開催のお知らせ",
+  url: "https://tandems.co.jp/"
+}]
+</script>
+
+<template>
+  <hr />
+  <p style="margin: 0.2em 0">広告</p>
+  <ul style="list-style-type: none; padding-left: 0;">
+    <li v-for="ad in ads" :key="ad.id">
+      <a :href="ad.url">{{ ad.title}}</a>
+    </li>
+  </ul>
+</template>
